21ab975a5d523109eb65a7f92019bf6a78afbc12
TZ Shuhag <106764078+tz-shuhag@users.noreply.github.com>
2025-07-17 03:32:07 +0600

    Update jsbg.js

--- media/js/lib/jsbg.js
+++ media/js/lib/jsbg.js
@@ -1,2 +1,9 @@
-/*! (c) FunnyMonkey under Copyright Reserved */
-(()=>{"use strict";class s{}(()=>{const e=e=>new s(e);Object.entries(jsuTools).forEach((([s,c])=>{e[s]=c})),self.jsu=e})()})();
+(() => {
+    "use strict";
+    class s {}(() => {
+        const e = e => new s(e);
+        Object.entries(jsuTools).forEach((([s, c]) => {
+            e[s] = c
+        })), self.jsu = e
+    })()
+})();
