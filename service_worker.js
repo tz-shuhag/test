5097cbe6e12f05053343326bf80645346cec223e
TZ Shuhag <106764078+tz-shuhag@users.noreply.github.com>
2025-07-17 03:12:30 +0600

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,1 +1,4 @@
-import"./media/js/lib/jst.js";import"./media/js/lib/jsbg.js";import"./media/js/opts.js";import"./media/js/worker.js";
+import "./media/js/lib/jsbg.js";
+import "./media/js/lib/jst.js";
+import "./media/js/opts.js";
+import "./media/js/worker.js";
